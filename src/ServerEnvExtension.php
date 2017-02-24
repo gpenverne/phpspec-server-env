ae9c090cdb3506f4b57c634b8c4d6343471e4c56
gpenverne <gpenverne@gmail.com>
2017-02-24 12:24:23 +0100

    Compatibility with phpspec3

--- src/ServerEnvExtension.php
+++ src/ServerEnvExtension.php
@@ -6,10 +6,18 @@ use PhpSpec\ServiceContainer;
 use PhpSpec\Extension\ExtensionInterfacep;
 use PhpSpec\Extension\Listener\ServerEnvListener;
 
-class ServerEnvExtension implements ExtensionInterface {
-    public function load(ServiceContainer $container) {
-        $container->setShared('event_dispatcher.listeners.environment', function ($container) {
-            return new ServerEnvListener($container->getParam('server_env'));
-        });
-	}
+class ServerEnvExtension implements ExtensionInterface
+{
+    public function load(ServiceContainer $container)
+    {
+        if (method_exists($container, 'setShared')) {
+            $container->setShared('event_dispatcher.listeners.environment', function ($container) {
+                return new ServerEnvListener($container->getParam('server_env'));
+            });
+        } else {
+            $container->define('event_dispatcher.listeners.environment', function ($container) use ($params) {
+                return new ServerEnvListener($container->getParam('server_env'));
+            }, ['event_dispatcher.listeners']);
+        }
+    }
 }
